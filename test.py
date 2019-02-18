f849d85954c8fcb6c184a62d677a26494f9d3e73
Anders Sjöbom <anders.sjobom@live.se>
2019-02-18 14:50:24 +0100

    chore: removed old "test file"

--- test.py
+++
@@ -1,7 +0,0 @@
-from application.finance import sma
-from application.finance import stock
-
-sma_values = sma.get_sma_values("ABB.ST", 10, "daily")
-stock_prices = stock.get_stock_price_close_history("ABB.ST")
-percent = sma.compare_sma_to_stock_price(sma, stock_prices, "2017-05-02")
-print(percent)
