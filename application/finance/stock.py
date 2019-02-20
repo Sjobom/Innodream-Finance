ea106b95778eee328efb9a0a058f8e0cc47912f9
Anders Sjöbom <anders.sjobom@live.se>
2019-02-21 00:04:19 +0100

    feat: fetch and store full stock history

--- application/finance/stock.py
+++ application/finance/stock.py
@@ -8,12 +8,21 @@ def get_single_day_history(ticker, date = str(date.today())):
     return http.get_json(single_day_history_url)
 
 
-def store_single_day_history(ticker, date, history):
+def store_single_day_history(ticker, date, single_day_history):
     mongo_db = db.get_mongo_db()
     history_data = {
         'ticker': ticker,
         'date': date,
-        'history': history
+        'history': single_day_history
     }
     return mongo_db.history.insert_one(history_data)
 
+
+def get_full_history(ticker):
+    history_url = urls.world_trading_data_full_history(ticker)
+    return http.get_json(history_url)
+
+
+def store_full_history(ticker, full_history):
+    for date, single_day_history in full_history['history'].items():
+        store_single_day_history(ticker, date, single_day_history)
--- application/http/urls.py
+++ application/http/urls.py
@@ -12,3 +12,11 @@ def world_trading_data_single_day_history(ticker, date):
     url = url.replace("{date}", date)
     url = url.replace("{token}", api_key)
     return url
+
+
+def world_trading_data_full_history(ticker):
+    api_key = config.get_stock_api_key()
+    url = 'https://www.worldtradingdata.com/api/v1/history?symbol={ticker}&sort=newest&api_token={token}'
+    url = url.replace('{ticker}', ticker)
+    url = url.replace('{token}', api_key)
+    return url
